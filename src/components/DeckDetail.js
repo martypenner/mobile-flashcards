7e749661c71c0b60ed5c30b64401d177ccdc5533
Marty Penner <penner.marty@gmail.com>
2017-12-01 23:41:13 -0500

    Wire up deck detail with an actual deck id and title

--- src/components/DeckDetail.js
+++ src/components/DeckDetail.js
@@ -1,51 +1,27 @@
-import { H1, Text } from 'native-base';
+import { H1 } from 'native-base';
 import React from 'react';
-import { SectionList, TouchableOpacity, View } from 'react-native';
-import {
-  componentFromStream,
-  compose,
-  setStatic,
-  hoistStatics
-} from 'recompose';
+import { View } from 'react-native';
+import { componentFromStream } from 'recompose';
 
 import { decks$ } from '../streams';
 
-const DeckDetail = componentFromStream(props$ => {
-  return props$.combineLatest(decks$.startWith(null), (_, decks) => (
-    <View style={{ marginTop: 50 }}>
-      <SectionList
-        sections={decks}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
-        renderSectionHeader={({ section }) => (
-          <TouchableOpacity
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderBottomWidth: 1,
-              borderColor: 'black',
-              padding: 20
-            }}>
-            <H1>{section.title}</H1>
-
-            <Text style={{ fontSize: 15, color: 'gray' }}>
-              {section.data.length} card{section.data.length === 0 ||
-              section.data.length > 1
-                ? 's'
-                : ''}
-            </Text>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
-  ));
-});
+const DeckDetail = componentFromStream(props$ =>
+  props$.switchMap(props =>
+    decks$
+      .map(decks =>
+        decks.find(deck => props.navigation.state.params.id === deck.key)
+      )
+      .tag('deck detail')
+      .map(deck => (
+        <View>
+          <H1>{deck.title}</H1>
+        </View>
+      ))
+  )
+);
 
 DeckDetail.navigationOptions = ({ navigation }) => ({
   headerTitle: navigation.state.params.title
 });
-
-// export default compose(
-//   setStatic('navigationOptions', (props) => console.info(props) || { headerTitle: 'alskdfjsdlkfj' })
-// )(DeckDetail);
 
 export default DeckDetail;
--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -5,41 +5,38 @@ import { componentFromStream } from 'recompose';
 
 import { decks$ } from '../streams';
 
-export default componentFromStream(props$ => {
-  return props$.combineLatest(
-    decks$.startWith(null),
-    ({ navigation }, decks) => (
-      <View style={{ marginTop: 50 }}>
-        <SectionList
-          sections={decks}
-          renderItem={({ item }) => <Text>{item.title}</Text>}
-          renderSectionHeader={({ section }) => (
-            <TouchableOpacity
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                borderBottomWidth: 1,
-                borderColor: 'black',
-                padding: 20
-              }}
-              onPress={() =>
-                navigation.navigate('DeckDetail', {
-                  id: section.key,
-                  title: section.title
-                })
-              }>
-              <H1>{section.title}</H1>
+export default componentFromStream(props$ =>
+  props$.combineLatest(decks$.startWith(null), ({ navigation }, decks) => (
+    <View style={{ marginTop: 50 }}>
+      <SectionList
+        sections={decks}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderSectionHeader={({ section }) => (
+          <TouchableOpacity
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderBottomWidth: 1,
+              borderColor: 'black',
+              padding: 20
+            }}
+            onPress={() =>
+              navigation.navigate('DeckDetail', {
+                id: section.key,
+                title: section.title
+              })
+            }>
+            <H1>{section.title}</H1>
 
-              <Text style={{ fontSize: 15, color: 'gray' }}>
-                {section.data.length} card{section.data.length === 0 ||
-                section.data.length > 1
-                  ? 's'
-                  : ''}
-              </Text>
-            </TouchableOpacity>
-          )}
-        />
-      </View>
-    )
-  );
-});
+            <Text style={{ fontSize: 15, color: 'gray' }}>
+              {section.data.length} card{section.data.length === 0 ||
+              section.data.length > 1
+                ? 's'
+                : ''}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  ))
+);
