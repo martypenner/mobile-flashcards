7fbc07d3c2c65ab59fb23c68c2f2603c2ddb8d08
Marty Penner <penner.marty@gmail.com>
2017-12-04 10:14:02 -0500

    Add an empty state for deck list

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -9,30 +9,37 @@ import NumCards from './NumCards';
 export default componentFromStream(props$ =>
   props$.combineLatest(decks$, ({ navigation }, decks) => (
     <View style={{ marginTop: 50 }}>
-      <SectionList
-        sections={Object.values(decks)}
-        renderItem={() => null}
-        renderSectionHeader={({ section }) => (
-          <TouchableOpacity
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderBottomWidth: 1,
-              borderColor: 'black',
-              padding: 20
-            }}
-            onPress={() =>
-              navigation.navigate('DeckDetail', {
-                deckId: section.key,
-                title: section.title
-              })
-            }>
-            <H1>{section.title}</H1>
+      {Object.values(decks).length > 0 ? (
+        <SectionList
+          sections={Object.values(decks)}
+          renderItem={() => null}
+          renderSectionHeader={({ section }) => (
+            <TouchableOpacity
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderBottomWidth: 1,
+                borderColor: 'black',
+                padding: 20
+              }}
+              onPress={() =>
+                navigation.navigate('DeckDetail', {
+                  deckId: section.key,
+                  title: section.title
+                })
+              }>
+              <H1>{section.title}</H1>
 
-            <NumCards cards={section.data} />
-          </TouchableOpacity>
-        )}
-      />
+              <NumCards cards={section.data} />
+            </TouchableOpacity>
+          )}
+        />
+      ) : (
+        <Text style={{ marginHorizontal: 10, textAlign: 'center' }}>
+          It looks like you don't have any decks! Add some now so you can start
+          quizzing yourself!
+        </Text>
+      )}
     </View>
   ))
 );
